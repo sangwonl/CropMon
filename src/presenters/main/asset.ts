2096ad32defb132579881d6d07b16c20f5a8028a
gamz <gamzabaw@gmail.com>
2021-04-21 00:40:26 +0900

    Add AppTray with per platform menu builder

--- src/presenters/main/asset.ts
+++ src/presenters/main/asset.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 const RESOURCES_PATH = app.isPackaged
   ? path.join(process.resourcesPath, 'assets')
-  : path.join(__dirname, '../../assets');
+  : path.join(__dirname, '../../../assets');
 
 export const assetResolver = (...paths: string[]): string => {
   return path.join(RESOURCES_PATH, ...paths);
--- src/presenters/redux/store-main.ts
+++ src/presenters/redux/store-main.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/prefer-default-export */
 
-import store, { sagaMiddleware } from './store';
+import store, { RootState, sagaMiddleware } from './store';
 
 import captureSaga from './capture/saga';
 
 export const initializeSaga = () => {
   (store as any).saga = sagaMiddleware.run(captureSaga);
 };
+
+export { RootState };
 
 export default store;
