a9e0d2b428793390f2b834a3d47a30927b95efa6
gamz <gamzabaw@gmail.com>
2021-04-22 00:12:18 +0900

    Use utils to determine platform

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,3 +1,6 @@
+export const isMac = () => process.platform === 'darwin';
+export const isWin = () => process.platform === 'win32';
+
 export const isMain = () => process.type === 'browser';
 export const isRenderer = () => process.type === 'renderer';
 export const isWorker = () => process.type === 'worker';
