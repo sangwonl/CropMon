dc38e45dd5676a7d799611065e75f5ab3bbed1c3
gamz <gamzabaw@gmail.com>
2021-05-03 22:44:19 +0900

    Move mouse event handlers in capture area component

--- src/presenters/ui/overlays/Cover.tsx
+++ src/presenters/ui/overlays/Cover.tsx
@@ -3,27 +3,16 @@
 /* eslint-disable react/no-unused-prop-types */
 /* eslint-disable import/prefer-default-export */
 
-import React, { MouseEvent, useState, useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { RootState } from '@presenters/redux/store';
 import { IOverlaysWindows } from '@presenters/redux/ui/types';
 import { getCurWindowCustomData } from '@utils/custom';
 
-import { CaptureArea, AreaSelectionCtx } from './CaptureArea';
+import { CaptureArea } from './CaptureArea';
 
 import styles from './Cover.css';
-
-const initialSelectionCtx: AreaSelectionCtx = {
-  started: false,
-  selected: false,
-  startX: 0,
-  startY: 0,
-  endX: 0,
-  endY: 0,
-  curX: 0,
-  curY: 0,
-};
 
 const stretchBodySize = (w: number, h: number) => {
   document.body.style.width = `${w}px`;
@@ -34,41 +23,6 @@ export const Cover = () => {
   const overlaysWindows: IOverlaysWindows = useSelector(
     (state: RootState) => state.ui.overlaysWindows
   );
-
-  const [selectionCtx, setSelectionCtx] = useState<AreaSelectionCtx>(
-    initialSelectionCtx
-  );
-
-  const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-    setSelectionCtx({
-      ...selectionCtx,
-      started: true,
-      selected: false,
-      startX: e.clientX,
-      startY: e.clientY,
-      curX: e.clientX,
-      curY: e.clientY,
-    });
-  };
-
-  const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
-    setSelectionCtx({
-      ...selectionCtx,
-      selected: true,
-      endX: e.clientX,
-      endY: e.clientY,
-      curX: e.clientX,
-      curY: e.clientY,
-    });
-  };
-
-  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    setSelectionCtx({
-      ...selectionCtx,
-      curX: e.clientX,
-      curY: e.clientY,
-    });
-  };
 
   useLayoutEffect(() => {
     if (
@@ -84,13 +38,8 @@ export const Cover = () => {
   }, [overlaysWindows]);
 
   return (
-    <div
-      className={styles.cursor}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-    >
-      <CaptureArea ctx={selectionCtx} />
+    <div className={styles.cursor}>
+      <CaptureArea />
     </div>
   );
 };
