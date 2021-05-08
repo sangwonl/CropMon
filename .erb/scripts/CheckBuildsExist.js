a3e8b78a740e4a80a24ba2abdbf3d6688eadf98e
gamz <gamzabaw@gmail.com>
2021-05-08 22:09:46 +0900

    Fix broken tests

--- .erb/scripts/CheckBuildsExist.js
+++ .erb/scripts/CheckBuildsExist.js
@@ -3,23 +3,18 @@ import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
 
-const mainPath = path.join(__dirname, '../../src/dist/main.prod.js');
-const rendererPath = path.join(
-  __dirname, '../../src/dist/renderer.main.prod.js'
-);
+const buildPaths = [
+  path.join(__dirname, '../../src/dist/main.prod.js'),
+  path.join(__dirname, '../../src/dist/renderer.preferences.prod.js'),
+  path.join(__dirname, '../../src/dist/renderer.overlays.prod.js'),
+];
 
-if (!fs.existsSync(mainPath)) {
-  throw new Error(
-    chalk.whiteBright.bgRed.bold(
-      'The main process is not built yet. Build it by running "yarn build:main"'
-    )
-  );
-}
-
-if (!fs.existsSync(rendererPath)) {
-  throw new Error(
-    chalk.whiteBright.bgRed.bold(
-      'The renderer process is not built yet. Build it by running "yarn build:renderer"'
-    )
-  );
-}
+buildPaths.forEach((buildPath) => {
+  if (!fs.existsSync(buildPath)) {
+    throw new Error(
+      chalk.whiteBright.bgRed.bold(
+        'The main process is not built yet. Build it by running "yarn build"'
+      )
+    );
+  }
+});
