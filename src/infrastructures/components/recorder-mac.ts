b538d2903a68122b3c29f0916034349964c2a50e
gamz <gamzabaw@gmail.com>
2021-05-11 23:09:52 +0900

    Support recording with ffmpeg for MacOS

--- src/infrastructures/components/recorder-mac.ts
+++ src/infrastructures/components/recorder-mac.ts
@@ -1,21 +1,72 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
+import assert from 'assert';
+
+import { Display, screen } from 'electron';
+import { ChildProcess } from 'child_process';
+import Ffmpeg, { FfmpegCommand } from 'fluent-ffmpeg';
 import { injectable } from 'inversify';
 
-import { IScreenRecorder } from '@core/components';
 import { ICaptureContext } from '@core/entities/capture';
+import { IBounds } from '@core/entities/screen';
+import { IScreenRecorder } from '@core/components';
 
 @injectable()
 export class ScreenRecorderMac implements IScreenRecorder {
+  lastFfmpeg!: FfmpegCommand;
+
+  // eslint-disable-next-line class-methods-use-this
   async record(ctx: ICaptureContext): Promise<void> {
-    // eslint-disable-next-line prefer-promise-reject-errors
-    return Promise.reject(undefined);
+    const { screenId, bounds: targetBounds } = ctx.target;
+
+    const targetDisplay = this.getDisplay(screenId);
+    if (targetDisplay === undefined || targetBounds === undefined) {
+      return;
+    }
+
+    const screenIdx = this.getScreenIdx(targetDisplay);
+    const { x, y, width, height } = this.calcScaledBounds(
+      targetBounds,
+      targetDisplay
+    );
+
+    const ffmpeg = Ffmpeg()
+      .input(`${screenIdx}:none`)
+      .inputFormat('avfoundation')
+      .inputOptions(['-r 30', `-capture_cursor 1`])
+      .videoCodec('libx264')
+      .withVideoFilter(`crop=${width}:${height}:${x}:${y}`)
+      .withOptions(['-pix_fmt yuv420p'])
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      .save(ctx.outputPath!);
+
+    this.lastFfmpeg = ffmpeg;
   }
 
-  async finish(ctx: ICaptureContext): Promise<void> {
-    // eslint-disable-next-line prefer-promise-reject-errors
-    return Promise.reject(undefined);
+  async finish(_ctx: ICaptureContext): Promise<void> {
+    assert(this.lastFfmpeg !== undefined);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const proc = (this.lastFfmpeg as any).ffmpegProc as ChildProcess;
+    proc?.stdin?.write('q');
+  }
+
+  private getDisplay(screenId: number): Display | undefined {
+    return screen.getAllDisplays().find((d) => d.id === screenId);
+  }
+
+  private getScreenIdx(targetDisp: Display): number {
+    return screen.getAllDisplays().findIndex((d) => d.id === targetDisp.id) + 1;
+  }
+
+  private calcScaledBounds(bounds: IBounds, disp: Display): IBounds {
+    return {
+      x: bounds.x * disp.scaleFactor,
+      y: bounds.y * disp.scaleFactor,
+      width: bounds.width * disp.scaleFactor,
+      height: bounds.height * disp.scaleFactor,
+    };
   }
 }
