29a7bbf12013344a6d0c40d7fa94edc1cda2c893
gamz <gamzabaw@gmail.com>
2021-05-19 00:36:58 +0900

    Fix quit and update not working

--- src/presenters/main/updater.ts
+++ src/presenters/main/updater.ts
@@ -1,28 +1,31 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
 
-import { dialog } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 
 export class AppUpdater {
   constructor() {
     autoUpdater.logger = log;
+    autoUpdater.autoDownload = false;
 
     autoUpdater.on('update-available', this.onUpdateAvailable);
     autoUpdater.on('update-not-available', this.onUpdateNotAvailable);
     autoUpdater.on('update-downloaded', this.onUpdateDownloaded);
+  }
 
+  checkForUpdates() {
     autoUpdater.checkForUpdates();
   }
 
-  onUpdateAvailable() {
+  private onUpdateAvailable() {
     autoUpdater.downloadUpdate();
   }
 
-  onUpdateNotAvailable() {}
+  private onUpdateNotAvailable() {}
 
-  async onUpdateDownloaded() {
+  private async onUpdateDownloaded() {
     const result = await dialog.showMessageBox({
       title: 'Install Updates',
       message: 'Updates are ready to be installed.',
@@ -33,7 +36,15 @@ export class AppUpdater {
 
     const buttonId = result.response;
     if (buttonId === 0) {
+      this.closeAllWindows();
       setImmediate(() => autoUpdater.quitAndInstall());
     }
+  }
+
+  private closeAllWindows() {
+    app.removeAllListeners('window-all-closed');
+    BrowserWindow.getAllWindows().forEach((w) => {
+      w.removeAllListeners('close');
+    });
   }
 }
