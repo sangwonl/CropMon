98366512d7383e7c0fe319571a2e7fdfe6b17253
gamz <gamzabaw@gmail.com>
2021-05-18 22:39:01 +0900

    Add auto update flow

--- src/presenters/main/updater.ts
+++ src/presenters/main/updater.ts
@@ -1,11 +1,39 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
 
+import { dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 
 export class AppUpdater {
   constructor() {
     autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
+
+    autoUpdater.on('update-available', this.onUpdateAvailable);
+    autoUpdater.on('update-not-available', this.onUpdateNotAvailable);
+    autoUpdater.on('update-downloaded', this.onUpdateDownloaded);
+
+    autoUpdater.checkForUpdates();
+  }
+
+  onUpdateAvailable() {
+    autoUpdater.downloadUpdate();
+  }
+
+  onUpdateNotAvailable() {}
+
+  async onUpdateDownloaded() {
+    const result = await dialog.showMessageBox({
+      title: 'Install Updates',
+      message: 'Updates are ready to be installed.',
+      defaultId: 0,
+      cancelId: 1,
+      buttons: ['Install and Restart', 'Close'],
+    });
+
+    const buttonId = result.response;
+    if (buttonId === 0) {
+      setImmediate(() => autoUpdater.quitAndInstall());
+    }
   }
 }
