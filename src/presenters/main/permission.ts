fd9727e00a17f1825dca6544754e73527dc9706e
gamz <gamzabaw@gmail.com>
2021-05-22 02:13:10 +0900

    Dummy permission asker

---
+++ src/presenters/main/permission.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-useless-return */
+/* eslint-disable import/prefer-default-export */
+
+// import { systemPreferences } from 'electron';
+// import { openSystemPreferences } from 'electron-util';
+
+// import { isMac } from '@utils/process';
+
+export const initializePermissions = async () => {
+  return;
+
+  // if (!isMac()) {
+  //   return;
+  // }
+
+  // if (systemPreferences.getMediaAccessStatus('screen') === 'granted') {
+  //   return;
+  // }
+
+  // await openSystemPreferences('security', 'Privacy_ScreenCapture');
+};
