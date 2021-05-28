38b97eeba8085801b52fffad46214ec032d98cf4
gamz <gamzabaw@gmail.com>
2021-05-28 23:03:26 +0900

    Crop image with canvas for performance reason

--- src/infrastructures/components/electron-recorder/recorder-delegate/renderer.js
+++ src/infrastructures/components/electron-recorder/recorder-delegate/renderer.js
@@ -6,10 +6,7 @@ const MEDIA_MIME_TYPE = 'video/webm; codecs=h264';
 
 let recordedChunks = [];
 let mediaRecorder;
-
 let outputPath;
-let targetBounds;
-let displayBounds;
 
 const getTempOutputPath = () => {
   const fileName = window.injected.dateString('YYYYMMDDHHmmss');
@@ -40,41 +37,74 @@ const handleRecordStop = async (_event) => {
   const buffer = window.injected.newBuffer(await blob.arrayBuffer());
   window.injected.fsWriteFile(tempPath, buffer);
 
-  // await cropWithFfmpeg(tempPath);
-
   window.injected.ipcSend('recording-file-saved', { tempFilePath: tempPath });
 };
 
-window.injected.ipcOn('start-record', async (_event, data) => {
-  const { screenId, screenWidth, screenHeight } = data;
-
+const getTargetSource = async (screenId) => {
   const inputSources = await window.injected.getCapturerSources({
     types: ['screen'],
   });
+  return inputSources.find((s) => parseInt(s.display_id) === screenId);
+};
 
-  const targetSource = inputSources.find(
-    (s) => parseInt(s.display_id) === screenId
-  );
-  if (targetSource === undefined) {
-    window.injected.ipcSend('recording-failed', {
-      message: 'failed to find input source matched to screen id',
-    });
-  }
-
-  const constraints = {
+const getMediaConstraint = (sourceId, screenBounds) => {
+  return {
     audio: false,
     video: {
       mandatory: {
         chromeMediaSource: 'desktop',
-        chromeMediaSourceId: targetSource.id,
-        minWidth: screenWidth,
-        minHeight: screenHeight,
-        maxWidth: screenWidth,
-        maxHeight: screenHeight,
+        chromeMediaSourceId: sourceId,
+        minWidth: screenBounds.width,
+        minHeight: screenBounds.height,
+        maxWidth: screenBounds.width,
+        maxHeight: screenBounds.height,
       },
     },
   };
+};
 
+const withCanvasProcess = (stream, screenBounds, targetBounds) => {
+  const videoElem = document.getElementById('video');
+  videoElem.videoWidth = screenBounds.width;
+  videoElem.videoHeight = screenBounds.height;
+  videoElem.srcObject = stream;
+  videoElem.play();
+
+  const canvasElem = document.getElementById('canvas');
+  canvasElem.width = targetBounds.width;
+  canvasElem.height = targetBounds.width;
+
+  const canvasCtx = canvasElem.getContext('2d');
+  const render = () => {
+    canvasCtx.drawImage(
+      videoElem,
+      targetBounds.x,
+      targetBounds.y,
+      targetBounds.width,
+      targetBounds.height,
+      0,
+      0,
+      targetBounds.width,
+      targetBounds.height
+    );
+  };
+  setInterval(render, 33);
+
+  return canvasElem.captureStream(30);
+};
+
+window.injected.ipcOn('start-record', async (_event, data) => {
+  const { screenId, screenBounds, targetBounds } = data;
+
+  const targetSource = await getTargetSource(screenId);
+  if (targetSource === undefined) {
+    window.injected.ipcSend('recording-failed', {
+      message: 'failed to find input source matched to screen id',
+    });
+    return;
+  }
+
+  const constraints = getMediaConstraint(targetSource.id, screenBounds);
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   if (stream === undefined) {
     window.injected.ipcSend('recording-failed', {
@@ -83,11 +113,13 @@ window.injected.ipcOn('start-record', async (_event, data) => {
     return;
   }
 
-  recordedChunks = [];
-
-  mediaRecorder = new MediaRecorder(stream, { mimeType: MEDIA_MIME_TYPE });
+  const canvasStream = withCanvasProcess(stream, screenBounds, targetBounds);
+  const recorderOpts = { mimeType: MEDIA_MIME_TYPE };
+  mediaRecorder = new MediaRecorder(canvasStream, recorderOpts);
   mediaRecorder.ondataavailable = handleStreamDataAvailable;
   mediaRecorder.onstop = handleRecordStop;
+
+  recordedChunks = [];
   mediaRecorder.start();
 
   window.injected.ipcSend('recording-started', {});
@@ -102,8 +134,6 @@ window.injected.ipcOn('stop-record', async (_event, data) => {
   }
 
   outputPath = data.outputPath;
-  targetBounds = data.targetBounds;
-  displayBounds = data.displayBounds;
 
   mediaRecorder.stop();
 });
