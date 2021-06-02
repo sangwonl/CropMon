4c007b8d02aa4eeeb9e074d64e63d8aaa8e1ab5b
gamz <gamzabaw@gmail.com>
2021-06-02 13:28:11 +0900

    Reorganize stateless window and components

--- src/presenters/ui/stateless/containers/progressdialog/shared.ts
+++ src/presenters/ui/stateless/containers/progressdialog/shared.ts
@@ -6,21 +6,18 @@ export interface ProgressDialogButton {
   enableOnCompletion: boolean;
 }
 
-export interface ProgressDialogLayout {
-  width: number;
-  height: number;
-}
-
 export interface ProgressDialogOptions {
   title: string;
   message: string;
   button: ProgressDialogButton;
-  layout?: ProgressDialogLayout;
   timeout?: number;
 }
 
-export interface IpcEventDataSetProgress {
-  percent: number;
+export const IPC_EVENT_SET_PROGRESS = 'set-progress';
+export const IPC_EVENT_ON_BUTTON_CLICK = 'on-button-click';
+
+export interface IpcEventSetProgress {
+  progress: number;
 }
 
-export interface IpcEventDataProgressDone {}
+export interface IpcEventOnButtonClick {}
