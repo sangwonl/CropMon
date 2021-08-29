fd405279fb1f853cae76aa22a2e391e18c5ac628
gamz <gamzabaw@gmail.com>
2021-08-29 12:56:50 +0900

    Increase framerate to about 24

--- src/infrastructures/recorder/recorder.ts
+++ src/infrastructures/recorder/recorder.ts
@@ -39,7 +39,7 @@ import { IRecordContext, ITargetSlice } from './rec-delegate/types';
 
 type ScreenAndBoundsTuple = [IScreen, IBounds | undefined];
 
-const FRAMERATE = 21;
+const FRAMERATE = 25;
 const FRAMERATE_LOW = 17;
 const VIDEO_BITRATES_LOW = 700000;
 const SCALE_DOWN_FACTOR_LOW = 0.7;
