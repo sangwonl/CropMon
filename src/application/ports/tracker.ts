a1c99ba6be3b984eec8dc4c63767d5ee2dcbd362
gamz <gamzabaw@gmail.com>
2023-05-25 18:15:56 +0900

    Integrate mixpanel tracking

--- src/application/ports/tracker.ts
+++ src/application/ports/tracker.ts
@@ -1,8 +1,9 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface AnalyticsTracker {
   view(name: string): void;
-  event(category: string, action: string, cb?: () => void): void;
-  eventL(category: string, action: string, label: string, cb?: () => void): void;
-  eventLV(category: string, action: string, label: string, value: number, cb?: () => void): void;
+  event(category: string, action: string): void;
+  eventL(category: string, action: string, label: string): void;
+  eventLV(category: string, action: string, label: string, value: number): void;
+  eventLVS(category: string, action: string, lvs: any): void;
 }
